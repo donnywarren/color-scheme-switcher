b455f5566aaaa085d10a3064612bb3a468167329
Bruno Galvao <brunopgalvao@gmail.com>
2019-02-27 11:33:49 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-(function colorSwitcher () {
+const buttons = document.querySelectorAll('li')
 
-})()
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(event){
+        document.body.style.backgroundColor = event.target.className
+    })
+}
