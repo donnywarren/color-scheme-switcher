37ed1b59d73adb81b2e476b1d521ea326298d305
Bruno Galvao <brunopgalvao@gmail.com>
2019-02-27 11:34:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-(function colorSwitcher () {
+const redButton = document.querySelector('.js-buttons .red')
+const whiteButton = document.querySelector('.js-buttons .white')
+const blueButton = document.querySelector('.js-buttons .blue')
+const yellowButton = document.querySelector('.js-buttons .yellow')
 
-})()
+const changeRed = function(){
+    document.body.style.backgroundColor = "red"
+    document.body.style.color = "white"
+  }
+  const changeWhite = function(){
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+  }
+  const changeBlue = function(){
+    document.body.style.backgroundColor = "blue"
+    document.body.style.color = "white"
+  }
+  const changeYellow = function(){
+    document.body.style.backgroundColor = "yellow"
+    document.body.style.color = "black"
+  }
+
+redButton.addEventListener("click", changeRed)
+whiteButton.addEventListener("click", changeWhite)
+blueButton.addEventListener("click", changeBlue)
+yellowButton.addEventListener("click", changeYellow)
