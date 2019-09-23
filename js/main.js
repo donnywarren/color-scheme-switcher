246edbc31f155229b8125950423b85676080965f
brianflynn <bdflynny@gmail.com>
2019-09-23 12:14:11 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-(function colorSwitcher () {
+// First, select each button.
 
-})()
+
+
+
+
+
+// Second, write an event listener for each of the buttons you selected. You can use callback functions or anonymous functions.
+
+
+
+
+
+
